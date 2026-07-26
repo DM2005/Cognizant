57693355ebf5f10cf8405d543995be408d3f12d0
Devansh Mishra <mdevansh0109@gmail.com>
2026-07-26 16:14:51 +0530

    Completed Spring Hibernatelogging.level.org.springframework=INFO

---
+++ Week4/orm-learn/src/main/java/com/example/orm_learn/services/CountryService.java
@@ -0,0 +1,21 @@
+package com.example.orm_learn.services;
+
+import com.example.orm_learn.model.Country;
+import com.example.orm_learn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
