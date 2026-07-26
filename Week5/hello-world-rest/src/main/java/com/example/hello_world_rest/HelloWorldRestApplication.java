6b1f0d3d2a6a29d080bfb3d584d542e845a93b55
Devansh Mishra <mdevansh0109@gmail.com>
2026-07-27 01:49:19 +0530

    Completed Exercise 3 Hello World RESTFUL WebService

---
+++ Week5/hello-world-rest/src/main/java/com/example/hello_world_rest/HelloWorldRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.hello_world_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApplication.class, args);
+	}
+}
