b5127b18c5c64ab55cde81df87f00684ec90f1ba
Devansh Mishra <mdevansh0109@gmail.com>
2026-07-27 17:29:27 +0530

    Completed ReactJS HOL 1

---
+++ Week6/ReactJS-HOL1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <h1>Welcome the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
