71a92f68182595dda4661d8a3fb51ce74474ec8e
Devansh Mishra <mdevansh0109@gmail.com>
2026-07-28 10:11:12 +0530

    ReactJS-HOL 5 completed

---
+++ Week6/ReactJs-HOL5/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CohortDetails from './CohortDetails';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Cohorts Details</h1>
+      <CohortDetails />
+    </div>
+  );
+}
+
+export default App;
